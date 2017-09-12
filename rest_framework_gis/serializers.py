806195146648a135a283a43ff80b590ee952b3c1
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2017-09-12 15:49:32 -0400

    Make GeometryField deserialization more robust

--- rest_framework_gis/serializers.py
+++ rest_framework_gis/serializers.py
@@ -1,5 +1,6 @@
 # TODO: Set the JSON package in the settings; want to use ujson
 import json
+from django.contrib.gis.gdal.error import GDALException
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.db import models
 from rest_framework import serializers
@@ -10,7 +11,30 @@ class GeometryField(serializers.Field):
         return obj
 
     def to_internal_value(self, data):
-        return GEOSGeometry(json.dumps(data))
+        if isinstance(data, GEOSGeometry):
+            return data
+
+        # If the data is not already a string, try to dump it to a string using
+        # a JSON serializer.
+        if isinstance(data, (str, bytes)):
+            str_data = data
+        else:
+            try:
+                str_data = json.dumps(data)
+            except TypeError:
+                raise serializers.ValidationError('Value must be a geometry string, or JSON-serializable GeoJSON dict. Got: {!r}'.format(data))
+
+        # Try to construct a geometry from the string data, raising
+        # ValidationError if not possible.
+        try:
+            geom = GEOSGeometry(str_data)
+        except ValueError:
+            raise serializers.ValidationError('Value not recognized as WKT, EWKT, HEXEWKB, or GeoJSON: {!r}'.format(data))
+        except GDALException:
+            raise serializers.ValidationError('Value does not represent a valid geometry: {!r}'.format(data))
+
+        # Return the resulting geometry.
+        return geom
 
 
 class GeometryModelSerializer (serializers.ModelSerializer):
--- rest_framework_gis/tests.py
+++ rest_framework_gis/tests.py
@@ -1,5 +1,8 @@
 #-*- coding:utf-8 -*-
+from django.contrib.gis.geos import GEOSGeometry
 from django.test import TestCase
+from rest_framework.serializers import ValidationError
+from rest_framework_gis.serializers import GeometryField
 
 
 # TODO: Tests
@@ -8,3 +11,45 @@ from django.test import TestCase
 # * Using a GeometryField on a geom field in any serializer
 # * Using a GeoJSONRenderer on some data
 # * Using a FeatureRenderer on some data
+
+class GeometryFieldTests (TestCase):
+    def test_parsing_valid_values(self):
+        field = GeometryField()
+
+        # WKT string
+        geom = field.to_internal_value('POINT(1 0)')
+        self.assertEqual(geom, GEOSGeometry('POINT(1 0)'))
+
+        # GeoJSON string
+        geom = field.to_internal_value('{"type":"Point","coordinates":[1,0]}')
+        self.assertEqual(geom, GEOSGeometry('{"type":"Point","coordinates":[1,0]}'))
+
+        # GeoJSON dict
+        geom = field.to_internal_value({'type': 'Point', 'coordinates': [1, 0]})
+        self.assertEqual(geom, GEOSGeometry('{"type":"Point","coordinates":[1,0]}'))
+
+        # GEOS Geometry
+        geom = field.to_internal_value(GEOSGeometry('POINT(1 0)'))
+        self.assertEqual(geom, GEOSGeometry('POINT(1 0)'))
+
+    def test_parsing_invalid_values(self):
+        field = GeometryField()
+
+        # Invalid string
+        with self.assertRaises(ValidationError):
+            geom = field.to_internal_value('No geometry here')
+
+        # Invalid dictionary
+        with self.assertRaises(ValidationError):
+            geom = field.to_internal_value({'msg': 'No geometry here'})
+
+        # Non-dictionary
+        with self.assertRaises(ValidationError):
+            geom = field.to_internal_value(['No geometry here'])
+
+        # Non-JSON-encodable obejct
+        class CustomObject (object):
+            pass
+
+        with self.assertRaises(ValidationError):
+            geom = field.to_internal_value(CustomObject())
